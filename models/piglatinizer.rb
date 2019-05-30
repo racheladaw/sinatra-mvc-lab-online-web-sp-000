663c4fb61e51e528bbb15780625b799f1b24b33a
Rachel Williams <williamsrachelada@gmail.com>
2019-05-30 04:38:09 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,8 +3,10 @@ class PigLatinizer
   def piglatinize(string)
     arr = string.split(" ")
     arr.map do |word|
+      
       if ["a", "e", "i", "o", "u"].include?(word[0].downcase)
         word << "way"
+        
       else
         i = 0
         word.each_char do |letter|
@@ -13,20 +15,11 @@ class PigLatinizer
           end
           i +=1
         end
-      # if word.length > 1
         end_of_word = word.slice!(0, i)
-      #binding.pry
         word << end_of_word << "ay"
-      #binding.pry
-      #   if ["a", "e", "i", "o", "u"].include?(end_of_word[0])
-      #     word << "way"
-      #   else
-      #     word << "ay"
-      #   end
-      # else
-      #   word << "way"
-      # end
       end
+      
     end.join(" ")
   end
+  
 end
