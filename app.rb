6097fe43905728d5ad6cb2e41d1b4d69f1d3fdfe
Rachel Williams <williamsrachelada@gmail.com>
2019-05-30 04:34:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @word = Piglatinize.new
+    @word = PigLatinize.new
     @piglatinized_word = @word.piglatinize(params[:user_phrase])
     # erb :show
   end
