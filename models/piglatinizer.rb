de1dfa2b70822399c4c154269de59d0e14ba8e3b
Rachel Williams <williamsrachelada@gmail.com>
2019-05-30 03:46:07 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,7 +3,10 @@ class PigLatinizer
   def piglatinize(string)
     arr = string.split(" ")
     arr.each do |word|
-      
+      i = 0
+      word.each do |letter|
+        if ["a", "e", "i", "o", "u"].include?(letter)
+          binding.pry 
   end
 
 end
